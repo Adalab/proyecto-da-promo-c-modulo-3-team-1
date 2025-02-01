db9227e853370f61d10e5ece89351b707c35aa06
23lau <laura.goni.p@gmail.com>
2025-02-01 15:01:45 +0100

    añadimos archivo etl

---
+++ etl_main_proyecto3_team1.py
@@ -0,0 +1,6 @@
+# ETL - FUNCIONES - PROYECTO 3 - TEAM 1 - OPTIMIZACIÓN DEL TALENTO
+
+import etl_funciones_proyecto3_team1
+
+# Ejecutar el proceso ETL completo:
+etl_funciones_proyecto3_team1.etl_process()
